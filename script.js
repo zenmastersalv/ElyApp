3cf8c8377a146abed01c06003de087b110e8afb1
zenmastersalv <139191697+zenmastersalv@users.noreply.github.com>
2023-07-27 23:12:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -14,4 +14,13 @@ document.getElementById('medicineForm').addEventListener('submit', function(e) {
     resultText += 'Como cada empaque contiene ' + packageContent + ' unidades, necesitas ' + packagesNeeded + ' empaques para tener al menos ' + totalUnitsNeeded.toFixed(2) + ' unidades.';
     
     document.getElementById('result').innerText = resultText;
+
+    // Show the modal
+    var resultModal = new bootstrap.Modal(document.getElementById('resultModal'));
+    resultModal.show();
+});
+
+// Reset the form when the "Cerrar" button is clicked
+document.querySelector('.modal-footer .btn').addEventListener('click', function() {
+    document.getElementById('medicineForm').reset();
 });
